f6b93b79d30b25488fbe12970360c6153888846f
aksarnia <134660731+aksarnia@users.noreply.github.com>
2023-06-03 20:14:15 -0300

    0.2.0
    - Minor adjustments made to ball and tower locations to be more compatible with certain older Apple iPad hardware
    - Not tested yet with Apple iPhone 14 or Apple iPhone 14 Pro, but should work as it does with all Apple iPhone 13 models/sub-models
    - Ball now respawns after being shot to allow re-playability
    - BUG (FIXED): Ball was respawning when one already existed at pivot point. Only one ball spawns now.

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,12 +5,73 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private Rigidbody2D pivot;
+    [SerializeField] private float detachDelay;
+    [SerializeField] private float respawnDelay;
+
+    private Rigidbody2D currentBallRigidbody;
+    private SpringJoint2D currentBallSprintJoint;
+
+    private Camera mainCamera;
+    private bool isDragging;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+
+        SpawnNewBall();
+    }
+
     private void Update()
     {
-        if (!Touchscreen.current.primaryTouch.press.isPressed) { return; }
+        if (currentBallRigidbody == null) { return; }
+
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+
+            isDragging = false;
+
+            return;
+        }
+
+        isDragging = true;
+        currentBallRigidbody.isKinematic = true;
 
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
-        print (touchPosition);
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+        currentBallRigidbody.position = worldPosition;
+    }
+
+    private void SpawnNewBall()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, pivot.position, Quaternion.identity);
+
+        currentBallRigidbody = ballInstance.GetComponent<Rigidbody2D>();
+        currentBallSprintJoint = ballInstance.GetComponent<SpringJoint2D>();
+
+        currentBallSprintJoint.connectedBody = pivot;
+    }
+
+    private void LaunchBall()
+    {
+        currentBallRigidbody.isKinematic = false;
+        currentBallRigidbody = null;
+
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSprintJoint.enabled = false;
+        currentBallSprintJoint = null;
+
+        Invoke(nameof(SpawnNewBall), respawnDelay);
     }
 }
